5d50f1002842477d5cce6e6be007416e29db941a
NazarStyranec3 <nazarsturanec@gmail.com>
2025-11-04 01:47:13 +0200

    start

--- Shop_machinery/settings.py
+++ Shop_machinery/settings.py
@@ -130,4 +130,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'main', 'media')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
